6efc5cab4bd01e52b8a138370eab7fe0740a66e4
murilopf <mux.pfernandes@gmail.com>
2021-10-16 15:55:34 -0300

    componentizing header, content and sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface GenresProps {
+  genres: Genre[],
+  selectedGenreId: number,
+  clickGenreButton: (id: number) => void
+}
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ genres, selectedGenreId, clickGenreButton }: GenresProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => clickGenreButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
